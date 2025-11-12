1ef59ee3ac1751e2c00f8a584772c9755982fc41
v3ri0N <bijacik.adam@gmail.com>
2025-11-12 13:59:21 +0100

    [LH-1] Dodano szkielet projekt

---
+++ lend-a-hand-backend/main-web/src/main/java/pl/lendahand/LendAHandBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.lendahand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendAHandBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendAHandBackendApplication.class, args);
+    }
+
+}
---
+++ lend-a-hand-backend/main-web/src/test/java/pl/lendahand/LendAHandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lendahand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendAHandBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
