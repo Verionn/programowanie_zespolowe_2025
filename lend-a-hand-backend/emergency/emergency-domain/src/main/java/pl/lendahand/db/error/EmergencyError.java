f8bf41c94332a16c8677810c7d84c7aa49bd88f2
v3ri0N <bijacik.adam@gmail.com>
2025-11-15 13:58:12 +0100

    LH-12 Add fetching emergency

--- lend-a-hand-backend/emergency/emergency-domain/src/main/java/pl/lendahand/db/error/EmergencyError.java
+++ lend-a-hand-backend/emergency/emergency-domain/src/main/java/pl/lendahand/db/error/EmergencyError.java
@@ -4,6 +4,7 @@ import pl.lendahand.BaseError;
 
 public interface EmergencyError extends BaseError {
 
+    String FAILED_TO_FETCH_EMERGENCY = "Failed to fetch entity from the database";
     String FAILED_TO_SAVE_TO_DATABASE = "Failed to save entity to the database";
     String UNSUCCESSFUL_DATABASE_READ = "The attempt to read from the database was unsuccessful";
 
@@ -16,6 +17,12 @@ public interface EmergencyError extends BaseError {
     record DatabaseReadUnsuccessfulError(String message) implements EmergencyError {
         public DatabaseReadUnsuccessfulError() {
             this(UNSUCCESSFUL_DATABASE_READ);
+        }
+    }
+
+    record FailedToFetchEmergencyError(String message) implements EmergencyError {
+        public FailedToFetchEmergencyError() {
+            this(FAILED_TO_FETCH_EMERGENCY);
         }
     }
 }
--- lend-a-hand-backend/emergency/emergency-repository/src/main/java/pl/lendahand/EmergencyQuery.java
+++ lend-a-hand-backend/emergency/emergency-repository/src/main/java/pl/lendahand/EmergencyQuery.java
@@ -3,10 +3,14 @@ package pl.lendahand;
 public class EmergencyQuery {
 
     private static final String EMERGENCY_TABLE = "emergency_emergency";
+    private static final String ID_COLUMN = "id";
 
     public static final String SAVE_EMERGENCY = "INSERT INTO %s VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
             .formatted(EMERGENCY_TABLE);
 
     public static final String FETCH_EMERGENCIES = "SELECT * FROM %s"
             .formatted(EMERGENCY_TABLE);
+
+    public static final String FETCH_EMERGENCY = "SELECT * FROM %s WHERE %s = ?"
+            .formatted(EMERGENCY_TABLE, ID_COLUMN);
 }
