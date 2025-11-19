71b4dc531b4aa79f77a01f19fe70fb45e5199e38
Wiktor Ciszewski <wiktor.ciszewski7734@gmail.com>
2025-11-19 17:31:40 +0100

    LH-37 Add view for single crisis event

---
+++ lend-a-hand-frontend/components/clusterLib/ClusteredMapView.tsx
@@ -0,0 +1,130 @@
+import React, { useEffect, useState, useRef } from "react";
+import MapView, { Marker, Region } from "react-native-maps";
+import { View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
+import SuperCluster, { PointFeature, ClusterFeature } from "supercluster";
+import UserLocationButton from "./UserLocationButton";
+import {
+  calculateBBox,
+  returnMapZoom,
+  markerToGeoJSONFeature,
+} from "./mapHelpers";
+import { EmergencyType } from "@/utils/types/types";
+import { ApiService } from "@/utils/api/CRUD";
+import { endpoint } from "@/constants/variables";
+import { pinColors, tintColorLight } from "@/constants/Colors";
+import ClusterMarker from "./ClusterMarker";
+import { getWidthPercent } from "@/utils/function/functions";
+
+export const ClusteredMapView = () => {
+  const mapRef = useRef<MapView>(null);
+  const [clusters, setClusters] = useState<
+    (PointFeature<any> | ClusterFeature<any>)[]
+  >([]);
+  const [region, setRegion] = useState<Region | null>(null);
+  const [emergencies, setEmergencies] = useState<EmergencyType[]>([]);
+  const [userLocation, setUserLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+  const superClusterRef = useRef(
+    new SuperCluster({ radius: getWidthPercent(6.5), maxZoom: 16 })
+  );
+  console.log(getWidthPercent(6.5));
+  useEffect(() => {
+    const fetchDataAndLocation = async () => {
+      const response = await ApiService.get<{ emergencies: EmergencyType[] }>(
+        `${endpoint}emergencies`
+      );
+      setEmergencies(response.emergencies);
+
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") return;
+
+      const loc = await Location.getCurrentPositionAsync({});
+      setUserLocation({
+        latitude: loc.coords.latitude,
+        longitude: loc.coords.longitude,
+      });
+      setRegion({
+        latitude: loc.coords.latitude,
+        longitude: loc.coords.longitude,
+        latitudeDelta: 0.5,
+        longitudeDelta: 0.5,
+      });
+    };
+
+    fetchDataAndLocation();
+  }, []);
+
+  useEffect(() => {
+    if (region) {
+      const bbox = calculateBBox(region);
+      const zoom = returnMapZoom(region, bbox, 1);
+      superClusterRef.current.load(emergencies.map(markerToGeoJSONFeature));
+      const clusters = superClusterRef.current.getClusters(bbox, zoom);
+      setClusters(clusters);
+    }
+  }, [region, emergencies]);
+
+  const onRegionChangeComplete = (newRegion: Region) => {
+    setRegion(newRegion);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <MapView
+        ref={mapRef}
+        initialRegion={region!}
+        onRegionChangeComplete={onRegionChangeComplete}
+        style={styles.map}
+      >
+        {userLocation && (
+          <Marker
+            coordinate={userLocation}
+            title="Your Location"
+            pinColor={pinColors.USER_LOCATION}
+          />
+        )}
+        {clusters.map((cluster) => (
+          <ClusterMarker
+            key={cluster.id || cluster.properties.id}
+            geometry={cluster.geometry}
+            properties={cluster.properties}
+            clusterColor={tintColorLight}
+            onPress={() => {
+              const zoomLevel =
+                cluster.id !== undefined
+                  ? Math.min(
+                      superClusterRef.current.getClusterExpansionZoom(
+                        Number(cluster.id)
+                      ),
+                      16
+                    )
+                  : 16;
+
+              mapRef.current?.animateToRegion(
+                {
+                  latitude: cluster.geometry.coordinates[1],
+                  longitude: cluster.geometry.coordinates[0],
+                  latitudeDelta: region!.latitudeDelta / 2,
+                  longitudeDelta: region!.longitudeDelta / 2,
+                },
+                500
+              );
+            }}
+          />
+        ))}
+      </MapView>
+      <UserLocationButton mapRef={mapRef} userLocation={userLocation} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
+
+export default ClusteredMapView;
