9cf512837be0cfaf1b260a85f30f4fd188e47376
Wikcis <wiktor.ciszewski7734@gmail.com>
2025-12-12 17:09:42 +0100

    LH-48 Fix error on creation screen

--- lend-a-hand-frontend/app/(tabs)/EmergencyForm.tsx
+++ lend-a-hand-frontend/app/(tabs)/EmergencyForm.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Alert, Pressable, StyleSheet, Text, TextInput, View,} from "react-native";
+import {Alert, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View,} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
 import {ThemedText} from "@/components/ThemedText";
 import {Picker} from "@react-native-picker/picker";
@@ -7,14 +7,15 @@ import {DateTimePickerAndroid} from "@react-native-community/datetimepicker";
 import {faker} from "@faker-js/faker";
 import {useApiContext} from "@/utils/context/apiContext";
 import {EmergencyTypesWithTranslation} from "@/utils/types/types";
+import {ThemedBackground} from "@/components/ThemedBackground";
 
 export default function AddEmergencyForm() {
     const {addEmergency} = useApiContext();
-    const [title, setTitle] = useState("Przykładowe wydarzenie kryzysowe");
-    const [description, setDescription] = useState("Opis wydarzenia");
-    const [type, setType] = useState("CLEANUP_ASSISTANCE");
-    const [latitude, setLatitude] = useState("51.0");
-    const [longitude, setLongitude] = useState("19.0");
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [type, setType] = useState("");
+    const [latitude, setLatitude] = useState("");
+    const [longitude, setLongitude] = useState("");
     const [dateTime, setDateTime] = useState(new Date());
 
     const handleValueChange = (itemValue: string) => {
@@ -83,107 +84,125 @@ export default function AddEmergencyForm() {
     };
 
     return (
-        <SafeAreaView style={{flex: 1}}>
-            <View style={styles.container}>
-                <Text style={styles.label}>Tytuł</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Wpisz tytuł"
-                    value={title}
-                    onChangeText={setTitle}
-                />
-
-                <Text style={styles.label}>Opis</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Wpisz opis"
-                    value={description}
-                    onChangeText={setDescription}
-                />
-
-                <Text style={styles.label}>Typ</Text>
-                <View style={styles.pickerContainer}>
-                    <Picker
-                        selectedValue={type}
-                        onValueChange={(itemValue) => handleValueChange(itemValue)}
-                        mode="dropdown"
-                    >
-                        {EmergencyTypesWithTranslation.map((option) => (
-                            <Picker.Item key={option.value} label={option.label} value={option.value}/>
-                        ))}
-                    </Picker>
+        <ThemedBackground style={{flex: 1}}>
+            <ScrollView style={styles.container} keyboardShouldPersistTaps={"handled"}>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Tytuł</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Wpisz tytuł"
+                        value={title}
+                        onChangeText={setTitle}
+                    />
                 </View>
 
-                <Text style={styles.label}>Szerokość geograficzna</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Wpisz szerokość geograficzną"
-                    value={latitude}
-                    onChangeText={setLatitude}
-                    keyboardType="numeric"
-                />
 
-                <Text style={styles.label}>Długość geograficzna</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Wpisz długość geograficzną"
-                    value={longitude}
-                    onChangeText={setLongitude}
-                    keyboardType="numeric"
-                />
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Opis</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Wpisz opis"
+                        value={description}
+                        onChangeText={setDescription}
+                    />
+                </View>
 
-                <Text style={styles.label}>Data i godzina rozpoczęcia</Text>
-                <Pressable onPress={() => openDateTimePicker("date", "calendar")} style={styles.input}>
-                    <Text>{formatDateTime(dateTime)}</Text>
-                </Pressable>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Typ</Text>
+                    <View style={styles.pickerContainer}>
+                        <Picker
+                            selectedValue={type}
+                            onValueChange={handleValueChange}
+                            style={{maxHeight: 50, minHeight: 50}}
+                        >
+                            {EmergencyTypesWithTranslation.map((option) => (
+                                <Picker.Item
+                                    key={option.value}
+                                    label={option.label}
+                                    value={option.value}
+                                />
+                            ))}
+                        </Picker>
+                    </View>
+                </View>
 
-                <Pressable onPress={handleSubmit} style={styles.submitButton}>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Szerokość geograficzna</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Wpisz szerokość geograficzną"
+                        value={latitude}
+                        onChangeText={setLatitude}
+                        keyboardType="numeric"
+                    />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Długość geograficzna</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Wpisz długość geograficzną"
+                        value={longitude}
+                        onChangeText={setLongitude}
+                        keyboardType="numeric"
+                    />
+                </View>
+
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Data i godzina rozpoczęcia</Text>
+                    <Pressable onPress={() => openDateTimePicker('date', 'calendar')} style={styles.input}>
+                        <Text>{formatDateTime(dateTime)}</Text>
+                    </Pressable>
+                </View>
+
+                <TouchableOpacity onPress={handleSubmit} style={styles.submitButton}>
                     <ThemedText style={styles.buttonText}>Dodaj</ThemedText>
-                </Pressable>
-            </View>
-        </SafeAreaView>
+                </TouchableOpacity>
+            </ScrollView>
+        </ThemedBackground>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 16,
-        backgroundColor: "#fff",
-        borderRadius: 8,
+        flexGrow: 1,
+        paddingHorizontal: "5%",
+        paddingBottom: "5%",
     },
     label: {
-        fontSize: 12,
+        fontSize: 16,
         marginBottom: 8,
     },
     input: {
-        height: 40,
         borderColor: "#ddd",
         borderWidth: 1,
         marginBottom: 12,
         paddingHorizontal: 8,
         borderRadius: 4,
         justifyContent: "center",
+        minHeight: 50,
     },
     pickerContainer: {
-        justifyContent: "center",
-        borderWidth: 1,
         borderColor: "#ddd",
+        borderWidth: 1,
         borderRadius: 4,
+        justifyContent: "center",
         marginBottom: 12,
-        height: 50,
     },
-
     submitButton: {
         backgroundColor: "#1F41BB",
         alignItems: "center",
         justifyContent: "center",
-        height: 50,
+        minHeight: 50,
         borderRadius: 5,
-        marginTop: 20,
+        marginVertical: 20,
     },
     buttonText: {
         fontSize: 20,
         color: "white",
         fontWeight: "bold",
     },
+    inputContainer: {
+        minHeight: 80,
+    }
 });
--- lend-a-hand-frontend/components/ButtonNavigation.tsx
+++ lend-a-hand-frontend/components/ButtonNavigation.tsx
@@ -1,65 +1,64 @@
-import { EmergencyTypesWithTranslationEnum } from "@/utils/types/types";
-import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { useApiContext } from "@/utils/context/apiContext";
+import {EmergencyTypesWithTranslationEnum} from "@/utils/types/types";
+import React, {useState} from "react";
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {useApiContext} from "@/utils/context/apiContext";
 
 type TabNavigationType = {
-  item: EmergencyTypesWithTranslationEnum;
+    item: EmergencyTypesWithTranslationEnum;
 };
 
-export const ButtonNavigation: React.FC<TabNavigationType> = ({ item }) => {
-  const { setFilter } = useApiContext();
-  const [currentFilter, setCurrentFilter] = useState<EmergencyTypesWithTranslationEnum | null>(null);
+export const ButtonNavigation: React.FC<TabNavigationType> = ({item}) => {
+    const {selectedCategories, setSelectedCategories} = useApiContext();
 
-  const isActive = currentFilter?.value === item.value;
+    const isActive = selectedCategories.includes(item.value);
 
-  const handlePress = () => {
-    if (isActive) {
-      setCurrentFilter(null);
-      setFilter(null);
-    } else {
-      setCurrentFilter(item);
-      setFilter(item);
-    }
-  };
+    const handlePress = () => {
+        setSelectedCategories((prev) => {
+            if (prev.includes(item.value)) {
+                return prev.filter((cat => cat !== item.value))
+            } else {
+                return [...prev, item.value]
+            }
+        })
+    };
 
-  return (
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-            onPress={handlePress}
-            style={[
-              styles.button,
-              { backgroundColor: isActive ? "#FFFFFF" : "transparent" },
-              { borderColor: isActive ? "#FFFFFF" : "#000000" },
-            ]}
-        >
-          <Text
-              style={[
-                styles.buttonText,
-                { color: isActive ? "#000000" : "#000000" },
-              ]}
-          >
-            {item.label}
-          </Text>
-        </TouchableOpacity>
-      </View>
-  );
+    return (
+        <View style={styles.buttonContainer}>
+            <TouchableOpacity
+                onPress={handlePress}
+                style={[
+                    styles.button,
+                    {backgroundColor: isActive || selectedCategories.length === 0 ? "#FFFFFF" : "transparent"},
+                    {borderColor: isActive || selectedCategories.length === 0 ? "#FFFFFF" : "#000000"},
+                ]}
+            >
+                <Text
+                    style={[
+                        styles.buttonText,
+                        {color: isActive || selectedCategories.length === 0 ? "#000000" : "#000000"},
+                    ]}
+                >
+                    {item.label}
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    marginHorizontal: 10,
-  },
-  button: {
-    backgroundColor: "transparent",
-    borderRadius: 25,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    alignItems: "center",
-    borderWidth: 2,
-  },
-  buttonText: {
-    color: "#000000",
-    fontSize: 20,
-  },
+    buttonContainer: {
+        marginHorizontal: 10,
+    },
+    button: {
+        backgroundColor: "transparent",
+        borderRadius: 25,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        alignItems: "center",
+        borderWidth: 2,
+    },
+    buttonText: {
+        color: "#000000",
+        fontSize: 20,
+    },
 });
