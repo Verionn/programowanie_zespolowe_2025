af35a73efb0244c2f4cad85f7d64bc11cc79e9c4
v3ri0N <bijacik.adam@gmail.com>
2026-01-15 20:05:02 +0100

    LH-26 - Login implementation

--- lend-a-hand-backend/user/user-domain/src/main/java/pl/lendahand/facade/Users.java
+++ lend-a-hand-backend/user/user-domain/src/main/java/pl/lendahand/facade/Users.java
@@ -1,12 +1,16 @@
 package pl.lendahand.facade;
 
 import io.vavr.control.Either;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.lendahand.BaseError;
 import pl.lendahand.db.UserRepository;
 import pl.lendahand.error.UserDomainError;
 import pl.lendahand.model.User;
+import pl.lendahand.security.JwtService;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class Users {
@@ -27,6 +31,20 @@ public class Users {
 
         return userRepository.save(USER_MAPPER.userToUserEntity(user), passwordEncoder)
                 .map(response -> user);
+    }
+
+    public Either<BaseError, String> login(
+            String email,
+            String password,
+            AuthenticationManager authenticationManager,
+            JwtService jwtService) {
+
+        return userRepository.findByEmail(email)
+                .filterOrElse(Objects::nonNull, error -> new UserDomainError.UserNotFound())
+                .map(user -> {
+                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.id(), password));
+                    return jwtService.generateToken(USER_MAPPER.userEntityToUser(user));
+                });
     }
 
     public Either<BaseError, User> getUser(UUID userId) {
--- lend-a-hand-backend/user/user-web/src/main/java/pl/lendahand/UserController.java
+++ lend-a-hand-backend/user/user-web/src/main/java/pl/lendahand/UserController.java
@@ -2,11 +2,14 @@ package pl.lendahand;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 import pl.lendahand.facade.Users;
+import pl.lendahand.model.LoginRequest;
 import pl.lendahand.model.RegisterRequest;
 import pl.lendahand.model.User;
 import pl.lendahand.model.UserControllerMapper;
+import pl.lendahand.security.JwtService;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -17,11 +20,15 @@ import static org.springframework.http.HttpStatus.OK;
 @RestController
 public class UserController {
 
+    private final JwtService jwtService;
     private final UserControllerMapper userControllerMapper = UserControllerMapper.INSTANCE;
     private final Users users;
+    private final AuthenticationManager authenticationManager;
 
-    public UserController(Users users) {
+    public UserController(JwtService jwtService, Users users, AuthenticationManager authenticationManager) {
+        this.jwtService = jwtService;
         this.users = users;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/registration")
@@ -35,6 +42,21 @@ public class UserController {
                         UserResponseSolver::resolveError,
                         response -> ResponseEntity.status(CREATED).body(
                                 userControllerMapper.userToRegisterResponse(response)
+                        )
+                );
+    }
+
+    @PostMapping("/login")
+    ResponseEntity<?> login(@Valid @RequestBody LoginRequest authenticationRequest) {
+        return users.login(
+                        authenticationRequest.email(),
+                        authenticationRequest.password(),
+                        authenticationManager,
+                        jwtService)
+                .fold(
+                        UserResponseSolver::resolveError,
+                        response -> ResponseEntity.status(CREATED).body(
+                                userControllerMapper.tokenToAuthenticationResponse(response)
                         )
                 );
     }
